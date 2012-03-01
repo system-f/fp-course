7833488c6ee066a13aae32e9f2d1395ae6dc5dbb
Tony Morris <tmorris@tmorris.net>
2012-03-01 10:38:10 +1000

    demo

---
+++ demo/src/M/Demo.hs
@@ -0,0 +1,9 @@
+module M.Demo where
+
+import M
+
+add7and8 ::
+  Int
+add7and8 =
+  add 7 8
+
---
+++ demo/src/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import M.Demo
+
+main ::
+  IO ()
+main = 
+  putStrLn ("add 7 and 8: " ++ show add7and8)
+
