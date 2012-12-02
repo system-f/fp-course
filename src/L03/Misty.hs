9c31b97b539832e2207aa3ff9d9f5649f39b8f2d
Tony Morris <tmorris@tmorris.net>
2012-12-03 08:05:19 +1000

    reorder misty exercises

--- src/L03/Misty.hs
+++ src/L03/Misty.hs
@@ -44,47 +44,47 @@ jellybean :: Misty m => m (m a) -> m a
 jellybean = error "todo"
 
 -- Exercise 10
+-- Relative Difficulty: 10
+apple :: Misty m => m (a -> b) -> m a -> m b
+apple = error "todo"
+
+-- Exercise 11
 -- Relative Difficulty: 6
 -- (bonus: use apple + furry')
 lemon2 :: Misty m => (a -> b -> c) -> m a -> m b -> m c
 lemon2 = error "todo"
 
--- Exercise 11
+-- Exercise 12
 -- Relative Difficulty: 6
 -- (bonus: use apple + lemon2)
 lemon3 :: Misty m => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
 lemon3 = error "todo"
 
--- Exercise 12
+-- Exercise 13
 -- Relative Difficulty: 6
 -- (bonus: use apple + lemon3)
 lemon4 :: Misty m => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e
 lemon4 = error "todo"
 
--- Exercise 13
+-- Exercise 14
 -- Relative Difficulty: 3
 sausage :: Misty m => [m a] -> m [a]
 sausage = error "todo"
 
--- Exercise 14
+-- Exercise 15
 -- Relative Difficulty: 3
 moppy :: Misty m => (a -> m b) -> [a] -> m [b]
 moppy = error "todo"
 
--- Exercise 15
+-- Exercise 16
 -- Relative Difficulty: 4
 rockstar :: Misty m => Int -> m a -> m [a]
 rockstar = error "todo"
 
--- Exercise 16
+-- Exercise 17
 -- Relative Difficulty: 9
 filtering  :: Misty m => (a -> m Bool) -> [a] -> m [a]
 filtering = error "todo"
-
--- Exercise 17
--- Relative Difficulty: 10
-apple :: Misty m => m (a -> b) -> m a -> m b
-apple = error "todo"
 
 -----------------------
 -- SUPPORT LIBRARIES --
--- src/L03/Misty.scala
+++ src/L03/Misty.scala
@@ -52,46 +52,46 @@ object Misty {
     sys.error("todo")
 
   // Exercise 10
-  // Relative Difficulty: 3
-  def sausage[M[_], A](x: List[M[A]])(implicit M: Misty[M]): M[List[A]] =
-    sys.error("todo")
-
-  // Exercise 11
-  // Relative Difficulty: 3
-  def moppy[M[_], A, B](f: A => M[B], x: List[A])(implicit M: Misty[M]): M[List[B]] =
-    sys.error("todo")
-
-  // Exercise 12
-  // Relative Difficulty: 4
-  def rockstar[M[_], A](n: Int, a: M[A])(implicit M: Misty[M]): M[List[A]] =
-    sys.error("todo")
-
-  // Exercise 13
-  // Relative Difficulty: 9
-  def filtering[M[_], A](f: A => M[Boolean], a: List[A])(implicit M: Misty[M]): M[List[A]] =
-    sys.error("todo")
-
-  // Exercise 14
   // Relative Difficulty: 10
   def apple[M[_], A, B](f: M[A => B], a: M[A])(implicit M: Misty[M]): M[B] =
     sys.error("todo")
 
-  // Exercise 15
+  // Exercise 11
   // Relative Difficulty: 6
   // (bonus: use apple + furry)
   def lemon2[M[_], A, B, C](f: A => B => C, a: M[A], b: M[B])(implicit M: Misty[M]): M[C] =
     sys.error("todo")
 
-  // Exercise 16
+  // Exercise 12
   // Relative Difficulty: 6
   // (bonus: use apple + furry)
   def lemon3[M[_], A, B, C, D](f: A => B => C => D, a: M[A], b: M[B], c: M[C])(implicit M: Misty[M]): M[D] =
     sys.error("todo")
 
-  // Exercise 17
+  // Exercise 13
   // Relative Difficulty: 6
   // (bonus: use apple + furry)
   def lemon4[M[_], A, B, C, D, E](f: A => B => C => D => E, a: M[A], b: M[B], c: M[C], d: M[D])(implicit M: Misty[M]): M[E] =
+    sys.error("todo")
+
+  // Exercise 14
+  // Relative Difficulty: 3
+  def sausage[M[_], A](x: List[M[A]])(implicit M: Misty[M]): M[List[A]] =
+    sys.error("todo")
+
+  // Exercise 15
+  // Relative Difficulty: 3
+  def moppy[M[_], A, B](f: A => M[B], x: List[A])(implicit M: Misty[M]): M[List[B]] =
+    sys.error("todo")
+
+  // Exercise 16
+  // Relative Difficulty: 4
+  def rockstar[M[_], A](n: Int, a: M[A])(implicit M: Misty[M]): M[List[A]] =
+    sys.error("todo")
+
+  // Exercise 17
+  // Relative Difficulty: 9
+  def filtering[M[_], A](f: A => M[Boolean], a: List[A])(implicit M: Misty[M]): M[List[A]] =
     sys.error("todo")
 
   ///////////////////////
