1ac3b443fd352d5d993eb598a84cf13f4521a0ec
Tony Morris <tmorris@tmorris.net>
2013-08-16 13:42:05 +1000

    Tic-Tac-Toe network server

---
+++ projects/TicTacToe-net/haskell/src/Network/TicTacToe.hs
@@ -0,0 +1,18 @@
+module Network.TicTacToe
+(
+  module Network.TicTacToe.Accept
+, module Network.TicTacToe.Command
+, module Network.TicTacToe.Env
+, module Network.TicTacToe.Game
+, module Network.TicTacToe.HandleLens
+, module Network.TicTacToe.Lens
+, module Network.TicTacToe.Ref
+) where
+
+import Network.TicTacToe.Accept
+import Network.TicTacToe.Command
+import Network.TicTacToe.Env
+import Network.TicTacToe.Game
+import Network.TicTacToe.HandleLens
+import Network.TicTacToe.Lens
+import Network.TicTacToe.Ref
