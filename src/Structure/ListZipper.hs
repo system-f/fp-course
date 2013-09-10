b80e9d3586a4f155f201f3807a781c46ad601b37
Tony Morris <tmorris@tmorris.net>
2013-09-10 10:31:34 +1000

    remove 'Relative difficulty' from list zipper exercises

--- src/Structure/ListZipper.hs
+++ src/Structure/ListZipper.hs
@@ -37,7 +37,6 @@ data MaybeListZipper a =
   deriving Eq
 
 -- Exercise 1
--- Relative Difficulty: 2
 --
 -- | Implement the `Functor` instance for `ListZipper`.
 --
@@ -48,7 +47,6 @@ instance Functor ListZipper where
     error "todo"
 
 -- Exercise 2
--- Relative Difficulty: 2
 --
 -- | Implement the `Functor` instance for `MaybeListZipper`.
 --
@@ -59,7 +57,6 @@ instance Functor MaybeListZipper where
     error "todo"
 
 -- Exercise 3
--- Relative Difficulty: 2
 --
 -- | Create a `MaybeListZipper` positioning the focus at the head.
 --
@@ -71,7 +68,6 @@ fromList =
   error "todo"
 
 -- Exercise 4
--- Relative Difficulty: 2
 --
 -- | Retrieve the `ListZipper` from the `MaybeListZipper` if there is one.
 --
@@ -104,7 +100,6 @@ instance ListZipper' MaybeListZipper where
     IsZ
 
 -- Exercise 5
--- Relative Difficulty: 2
 --
 -- | Convert the given zipper back to a list.
 toList ::
@@ -115,7 +110,6 @@ toList =
   error "todo"
 
 -- Exercise 6
--- Relative Difficulty: 3
 --
 -- | Update the focus of the zipper with the given function on the current focus.
 --
@@ -133,7 +127,6 @@ withFocus =
   error "todo"
 
 -- Exercise 7
--- Relative Difficulty: 2
 --
 -- | Set the focus of the zipper to the given value.
 -- /Tip:/ Use `withFocus`.
@@ -163,7 +156,6 @@ setFocus =
   flip setFocus
 
 -- Exercise 8
--- Relative Difficulty: 2
 --
 -- | Returns whether there are values to the left of focus.
 --
@@ -180,7 +172,6 @@ hasLeft =
   error "todo"
 
 -- Exercise 9
--- Relative Difficulty: 2
 --
 -- | Returns whether there are values to the right of focus.
 --
@@ -197,7 +188,6 @@ hasRight =
   error "todo"
 
 -- Exercise 10
--- Relative Difficulty: 3
 --
 -- | Seek to the left for a location matching a predicate, starting from the
 -- current one.
@@ -214,7 +204,6 @@ findLeft =
   error "todo"
 
 -- Exercise 11
--- Relative Difficulty: 3
 --
 -- | Seek to the right for a location matching a predicate, starting from the
 -- current one.
@@ -231,7 +220,7 @@ findRight =
   error "todo"
 
 -- Exercise 12
--- Relative Difficulty: 4
+--
 -- | Move the zipper left, or if there are no elements to the left, go to the far right.
 -- CAUTION: This function is non-total, why?
 --
@@ -248,7 +237,7 @@ moveLeftLoop =
   error "todo"
 
 -- Exercise 13
--- Relative Difficulty: 4
+--
 -- | Move the zipper right, or if there are no elements to the right, go to the far left.
 --
 -- >>> moveRightLoop (ListZipper [3,2,1] 4 [5,6,7])
@@ -264,7 +253,7 @@ moveRightLoop =
   error "todo"
 
 -- Exercise 14
--- Relative Difficulty: 3
+--
 -- | Move the zipper one position to the left.
 --
 -- >>> moveLeft (ListZipper [3,2,1] 4 [5,6,7])
@@ -280,7 +269,7 @@ moveLeft =
   error "todo"
 
 -- Exercise 15
--- Relative Difficulty: 3
+--
 -- | Move the zipper one position to the right.
 --
 -- >>> moveRight (ListZipper [3,2,1] 4 [5,6,7])
@@ -296,7 +285,7 @@ moveRight =
   error "todo"
 
 -- Exercise 16
--- Relative Difficulty: 3
+--
 -- | Swap the current focus with the value to the left of focus.
 --
 -- >>> swapLeft (ListZipper [3,2,1] 4 [5,6,7])
@@ -312,7 +301,7 @@ swapLeft =
   error "todo"
 
 -- Exercise 17
--- Relative Difficulty: 3
+--
 -- | Swap the current focus with the value to the right of focus.
 --
 -- >>> swapRight (ListZipper [3,2,1] 4 [5,6,7])
@@ -328,7 +317,7 @@ swapRight =
   error "todo"
 
 -- Exercise 18
--- Relative Difficulty: 3
+--
 -- | Drop all values to the left of the focus.
 --
 -- >>> dropLefts (ListZipper [3,2,1] 4 [5,6,7])
@@ -346,7 +335,7 @@ dropLefts =
   error "todo"
 
 -- Exercise 19
--- Relative Difficulty: 3
+--
 -- | Drop all values to the right of the focus.
 --
 -- >>> dropRights (ListZipper [3,2,1] 4 [5,6,7])
@@ -364,7 +353,7 @@ dropRights =
   error "todo"
 
 -- Exercise 20
--- Relative Difficulty: 4
+--
 -- Move the focus left the given number of positions. If the value is negative, move right instead.
 moveLeftN ::
   ListZipper' f =>
@@ -375,7 +364,7 @@ moveLeftN =
   error "todo"
 
 -- Exercise 21
--- Relative Difficulty: 4
+--
 -- Move the focus right the given number of positions. If the value is negative, move left instead.
 moveRightN ::
   ListZipper' f =>
@@ -386,7 +375,7 @@ moveRightN =
   error "todo"
 
 -- Exercise 22
--- Relative Difficulty: 6
+--
 -- | Move the focus left the given number of positions. If the value is negative, move right instead.
 -- If the focus cannot be moved, the given number of times, return the value by which it can be moved instead.
 --
@@ -413,7 +402,7 @@ moveLeftN' =
   error "todo"
 
 -- Exercise 23
--- Relative Difficulty: 6
+--
 -- | Move the focus right the given number of positions. If the value is negative, move left instead.
 -- If the focus cannot be moved, the given number of times, return the value by which it can be moved instead.
 --
@@ -440,7 +429,7 @@ moveRightN' =
   error "todo"
 
 -- Exercise 24
--- Relative Difficulty: 7
+--
 -- | Move the focus to the given absolute position in the zipper. Traverse the zipper only to the extent required.
 --
 -- >>> nth 1 (ListZipper [3,2,1] 4 [5,6,7])
@@ -460,7 +449,7 @@ nth =
   error "todo"
 
 -- Exercise 25
--- Relative Difficulty: 4
+--
 -- | Return the absolute position of the current focus in the zipper.
 --
 -- >>> index (ListZipper [3,2,1] 4 [5,6,7])
@@ -475,7 +464,7 @@ index =
   error "todo"
 
 -- Exercise 26
--- Relative Difficulty: 5
+--
 -- | Move the focus to the end of the zipper.
 -- CAUTION: This function is non-total, why?
 --
@@ -489,7 +478,7 @@ end =
   error "todo"
 
 -- Exercise 27
--- Relative Difficulty: 5
+--
 -- | Move the focus to the start of the zipper.
 --
 -- >>> start (ListZipper [3,2,1] 4 [5,6,7])
@@ -502,7 +491,7 @@ start =
   error "todo"
 
 -- Exercise 28
--- Relative Difficulty: 5
+--
 -- | Delete the current focus and pull the left values to take the empty position.
 --
 -- >>> deletePullLeft (ListZipper [3,2,1] 4 [5,6,7])
@@ -518,7 +507,7 @@ deletePullLeft =
   error "todo"
 
 -- Exercise 29
--- Relative Difficulty: 5
+--
 -- | Delete the current focus and pull the right values to take the empty position.
 --
 -- >>> deletePullRight (ListZipper [3,2,1] 4 [5,6,7])
@@ -534,7 +523,7 @@ deletePullRight =
   error "todo"
 
 -- Exercise 30
--- Relative Difficulty: 5
+--
 -- | Insert at the current focus and push the left values to make way for the new position.
 --
 -- >>> insertPushLeft 15 (ListZipper [3,2,1] 4 [5,6,7])
@@ -553,7 +542,7 @@ insertPushLeft =
   error "todo"
 
 -- Exercise 31
--- Relative Difficulty: 5
+--
 -- | Insert at the current focus and push the right values to make way for the new position.
 --
 -- >>> insertPushRight 15 (ListZipper [3,2,1] 4 [5,6,7])
@@ -611,7 +600,7 @@ instance Traversable [] where
     foldr (\a b -> fmap (:) (f a) <*> b) (unit [])
 
 -- Exercise 32
--- Relative Difficulty: 6
+--
 -- | Implement the `Apply` instance for `ListZipper`.
 -- This implementation zips functions with values by function application.
 --
@@ -622,7 +611,7 @@ instance Apply ListZipper where
     error "todo"
 
 -- Exercise 33
--- Relative Difficulty: 4
+--
 -- | Implement the `Apply` instance for `MaybeListZipper`.
 --
 -- /Tip:/ Use `<*>` for `ListZipper`.
@@ -631,7 +620,7 @@ instance Apply MaybeListZipper where
     error "todo"
 
 -- Exercise 34
--- Relative Difficulty: 5
+--
 -- | Implement the `Applicative` instance for `ListZipper`.
 -- This implementation produces an infinite list zipper (to both left and right).
 --
@@ -641,7 +630,7 @@ instance Applicative ListZipper where
     error "todo"
 
 -- Exercise 35
--- Relative Difficulty: 4
+--
 -- | Implement the `Applicative` instance for `MaybeListZipper`.
 --
 -- /Tip:/ Use @unit@ for `ListZipper`.
@@ -650,7 +639,7 @@ instance Applicative MaybeListZipper where
     error "todo"
 
 -- Exercise 36
--- Relative Difficulty: 7
+--
 -- | Implement the `Extend` instance for `ListZipper`.
 -- This implementation "visits" every possible zipper value derivable from a given zipper (i.e. all zippers to the left and right).
 --
@@ -663,7 +652,7 @@ instance Extend ListZipper where
     error "todo"
 
 -- Exercise 37
--- Relative Difficulty: 3
+--
 -- | Implement the `Comonad` instance for `ListZipper`.
 -- This implementation returns the current focus of the zipper.
 --
@@ -674,7 +663,7 @@ instance Comonad ListZipper where
     error "todo"
 
 -- Exercise 38
--- Relative Difficulty: 10
+--
 -- | Implement the `Traversable` instance for `ListZipper`.
 -- This implementation traverses a zipper while running some `Applicative` effect through the zipper.
 -- An effectful zipper is returned.
@@ -683,7 +672,7 @@ instance Traversable ListZipper where
     error "todo"
 
 -- Exercise 39
--- Relative Difficulty: 5
+--
 -- | Implement the `Traversable` instance for `MaybeListZipper`.
 --
 -- /Tip:/ Use `traverse` for `ListZipper`.
