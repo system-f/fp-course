4436d8585b847a40d3041179235cb8cad3101d57
Tony Morris <tmorris@tmorris.net>
2013-11-11 14:18:03 +1000

    hlint

--- src/Course.hs
+++ src/Course.hs
@@ -1,54 +1,28 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Course
-(
-  module Core
-, module Algorithm.Anagrams
-, module Algorithm.Cheque
-, module Algorithm.EditDistance
-, module Algorithm.FastAnagrams
-, module Intro.Id
-, module Intro.Optional
-, module Intro.Validation
-, module IO.Interactive
-, module Monad.Compose
-, module Monad.Functor
-, module Monad.Monad
-, module Monad.State
-, module Monad.StateT
-, module Parser.JsonParser
-, module Parser.JsonValue
-, module Parser.MoreParser
-, module Parser.Parser
-, module Parser.Person
-, module Structure.BKTree
-, module Structure.Lens
-, module Structure.List
-, module Structure.ListZipper
-, module Structure.MetricSpace
-) where
+module Course (module X) where
 
-import Core
-import Algorithm.Anagrams
-import Algorithm.Cheque
-import Algorithm.EditDistance
-import Algorithm.FastAnagrams
-import Intro.Id
-import Intro.Optional
-import Intro.Validation
-import IO.Interactive
-import Monad.Compose
-import Monad.Functor
-import Monad.Monad
-import Monad.State
-import Monad.StateT
-import Parser.JsonParser
-import Parser.JsonValue
-import Parser.MoreParser
-import Parser.Parser
-import Parser.Person
-import Structure.BKTree
-import Structure.Lens
-import Structure.List
-import Structure.ListZipper
-import Structure.MetricSpace
+import Core as X
+import Algorithm.Anagrams as X
+import Algorithm.Cheque as X
+import Algorithm.EditDistance as X
+import Algorithm.FastAnagrams as X
+import Intro.Id as X
+import Intro.Optional as X
+import Intro.Validation as X
+import IO.Interactive as X
+import Monad.Compose as X
+import Monad.Functor as X
+import Monad.Monad as X
+import Monad.State as X
+import Monad.StateT as X
+import Parser.JsonParser as X
+import Parser.JsonValue as X
+import Parser.MoreParser as X
+import Parser.Parser as X
+import Parser.Person as X
+import Structure.BKTree as X
+import Structure.Lens as X
+import Structure.List as X
+import Structure.ListZipper as X
+import Structure.MetricSpace as X
