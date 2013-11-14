095b33ddfa9b9a48ffb9dcb35baca0011055fd65
Tony Morris <tmorris@tmorris.net>
2013-11-14 13:13:52 +1000

    JsonValue

--- src/Course/JsonValue.hs
+++ src/Course/JsonValue.hs
@@ -5,13 +5,13 @@ module Course.JsonValue where
 import Course.Core
 import Course.List
 
-type Assoc = [(Str, JsonValue)]
+type Assoc = List (Str, JsonValue)
 
 data JsonValue =
      JsonString Str
    | JsonRational  Bool !Rational
    | JsonObject Assoc
-   | JsonArray  [JsonValue]
+   | JsonArray  (List JsonValue)
    | JsonTrue
    | JsonFalse
    | JsonNull
