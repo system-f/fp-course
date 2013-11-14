fc4973bef9f27bffdd6783ea3d13719b80765330
Tony Morris <tmorris@tmorris.net>
2013-11-14 13:12:14 +1000

    readFloat

--- src/Course/JsonParser.hs
+++ src/Course/JsonParser.hs
@@ -44,7 +44,7 @@ jsonString =
 -- Exercise 2
 -- | Parse a JSON rational.
 --
--- /Tip:/ Use @Numeric#readFloats@.
+-- /Tip:/ Use @readFloats@.
 --
 -- >>> parse jsonNumber "234"
 -- Result >< 234 % 1
