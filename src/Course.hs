102639a0c447ec072754d716bcb8489ee69b54fa
Tony Morris <tmorris@tmorris.net>
2013-11-15 11:01:48 +1000

    .

--- src/Course.hs
+++ src/Course.hs
@@ -3,30 +3,9 @@
 module Course (module X) where
 
 import Course.Core as X
-{-
-import Algorithm.Anagrams as X
-import Algorithm.Cheque as X
-import Algorithm.EditDistance as X
-import Algorithm.FastAnagrams as X
--}
 import Course.Id as X
 import Course.Optional as X
 import Course.Validation as X
-{-
-import IO.Interactive as X
-import Monad.Compose as X
-import Monad.Functor as X
-import Monad.Monad as X
-import Monad.State as X
-import Monad.StateT as X
-import Parser.JsonParser as X
-import Parser.JsonValue as X
-import Parser.MoreParser as X
-import Parser.Parser as X
-import Parser.Person as X
-import Structure.BKTree as X
-import Structure.Lens as X
--}
 import Course.List as X
 import Course.Functor as X
 import Course.Apply as X
@@ -49,7 +28,3 @@ import Course.Interactive as X
 import Course.Anagrams as X
 import Course.Cheque as X
 import Course.FastAnagrams as X
-{-
-import Structure.ListZipper as X
-import Structure.MetricSpace as X
--}
