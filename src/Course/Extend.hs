736347c792a201b6695b781891f95080a84cad69
Tony Morris <tmorris@tmorris.net>
2013-11-15 12:32:13 +1000

    .

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -16,20 +16,54 @@ class Functor f => Extend f where
 
 infixr 1 <<=
 
+-- | Implement the @Extend@ instance for @Id@.
+--
+-- >>> id <<= Id 7
+-- Id (Id 7)
 instance Extend Id where
   f <<= i =
     Id (f i)
 
+-- | Implement the @Extend@ instance for @List@.
+--
+-- >>> length <<= ('a' :. 'b' :. 'c' :. Nil)
+-- [3,2,1]
+--
+-- >>> id <<= (1 :. 2 :. 3 :. 4 :. Nil)
+-- [[1,2,3,4],[2,3,4],[3,4],[4]]
+--
+-- > reverse =<< ((1 :. 2 :. 3 :. Nil) :. (4 :. 5 :. 6 :. Nil) :. Nil)
+-- [3,2,1,6,5,4]
 instance Extend List where
   _ <<= Nil =
     Nil
   f <<= x@(_ :. t) =
     f x :. (f <<= t)
 
+-- | Implement the @Extend@ instance for @Optional@.
+--
+-- >>> id <<= (Full 7)
+-- Full (Full 7)
+--
+-- >>> id <<= Empty
+-- Empty
 instance Extend Optional where
   f <<= o =
     f . Full <$> o
 
+-- | Duplicate the functor using extension.
+--
+-- >>> cojoin (Id 7)
+-- Id (Id 7)
+--
+-- >>> cojoin (1 :. 2 :. 3 :. 4 :. Nil)
+-- [[1,2,3,4],[2,3,4],[3,4],[4]]
+--
+-- >>> cojoin (Full 7)
+-- Full (Full 7)
+--
+-- >>> cojoin Empty
+-- Empty
 cojoin ::
   Extend f =>
   f a
