6506e02222b621f6c301afccd53c79f9b2f63354
Daniel Velkov <norcobg@gmail.com>
2014-02-13 12:06:45 -0800

    Add basic tests for 'start' and 'end' functions

--- src/Course/ListZipper.hs
+++ src/Course/ListZipper.hs
@@ -477,6 +477,10 @@ index =
 --
 -- >>> end (zipper [3,2,1] 4 [5,6,7])
 -- [6,5,4,3,2,1] >7< []
+--
+-- prop> toList lz == toList (end lz)
+--
+-- prop> rights (end lz) == Nil
 end ::
   ListZipper a
   -> ListZipper a
@@ -487,6 +491,10 @@ end =
 --
 -- >>> start (zipper [3,2,1] 4 [5,6,7])
 -- [] >1< [2,3,4,5,6,7]
+--
+-- prop> toList lz == toList (start lz)
+--
+-- prop> lefts (start lz) == Nil
 start ::
   ListZipper a
   -> ListZipper a
