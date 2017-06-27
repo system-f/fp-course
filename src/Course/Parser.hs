78781f2383d9e857569f48d901d188fdc184edcb
Tony Morris <tmorris@tmorris.net>
2017-06-27 14:01:05 +1000

    instance Functor ParseResult. #196

--- src/Course/Parser.hs
+++ src/Course/Parser.hs
@@ -50,6 +50,12 @@ instance Show a => Show (ParseResult a) where
   show (Result i a) =
     stringconcat ["Result >", hlist i, "< ", show a]
 
+instance Functor ParseResult where
+  _ <$> ErrorResult e =
+    ErrorResult e
+  f <$> Result i a =
+    Result i (f a)
+
 -- Function to determine is a parse result is an error.
 isErrorResult ::
   ParseResult a
