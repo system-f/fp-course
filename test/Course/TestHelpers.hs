e0d4c229c8426993f5cec668ff55dab7070c7b91
Andrew McCluskey <andrew.mccluskey@csiro.au>
2017-06-27 13:50:57 +1000

    Get tests working in GHCi

--- test/Course/TestHelpers.hs
+++ test/Course/TestHelpers.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Course.TestHelpers where
+
+import Course.Core (Integer, (+))
 
 -- Specialise on Integer to avoid type annotations everywhere
 (+:) :: Integer -> Integer -> Integer
