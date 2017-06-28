b12c0171a12369f8f9ce6c6e2c9a5802230ee9a5
Andrew McCluskey <andrew.mccluskey@csiro.au>
2017-06-28 15:52:47 +1000

    Moar Functor tests

--- test/Course/FunctorTest.hs
+++ test/Course/FunctorTest.hs
@@ -1,21 +1,27 @@
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Course.FunctorTest where
 
-import           Test.Tasty       (TestTree, testGroup)
-import           Test.Tasty.HUnit (testCase, (@?=))
+import           Test.Tasty            (TestTree, testGroup)
+import           Test.Tasty.HUnit      (testCase, (@?=))
+import           Test.Tasty.QuickCheck (testProperty)
 
 import           Course.Core
-import           Course.Functor   ((<$>))
-import           Course.Id        (Id (..))
-import           Course.List      (List (..))
+import           Course.Functor        ((<$), (<$>))
+import           Course.Id             (Id (..))
+import           Course.List           (List (..))
+import           Course.Optional       (Optional (..))
 
 test_Functor :: TestTree
 test_Functor =
   testGroup "Functor" [
     idTest
   , listTest
+  , optionalTest
+  , functionTest
+  , anonMapTest
   ]
 
 idTest :: TestTree
@@ -27,10 +33,28 @@ listTest =
   testGroup "List" [
     testCase "empty list" $
       (+1) <$> Nil @?= Nil
+  , testCase "increment" $
+      (+1) <$> (1 :. 2 :. 3 :. Nil) @?= (2 :. 3 :. 4 :. Nil)
   ]
 
--- Test :: TestTree
--- Test =
---   testGroup "" [
+optionalTest :: TestTree
+optionalTest =
+  testGroup "Optional" [
+    testCase "Empty" $ (+1) <$> Empty @?= Empty
+  , testCase "Full"  $ (+1) <$> Full 2 @?= Full 3
+  ]
 
---   ]
+functionTest :: TestTree
+functionTest =
+  testCase "(->)" $ ((+1) <$> (*2)) 8 @?= 17
+
+
+anonMapTest :: TestTree
+anonMapTest =
+  testGroup "(<$)" [
+    testCase "Map 7" $ 7 <$ (1 :. 2 :. 3 :. Nil) @?= (7 :. 7 :. 7 :. Nil)
+  , testProperty "Always maps a constant value over List" $
+      \x a b c -> (x :: Integer) <$ ((a :. b :. c :. Nil) :: List Integer) == (x :. x :. x :. Nil)
+  , testProperty "Always maps a constant value over Full (Optional)" $
+      \(x :: Integer) (q :: Integer) -> x <$ Full q == Full x
+  ]
