6a8a32e05881f19b88c38837022bf0b9a3cb2743
Andrew McCluskey <andrew.mccluskey@csiro.au>
2017-06-29 09:21:26 +1000

    Add some real list tests

--- test/Course/ListTest.hs
+++ test/Course/ListTest.hs
@@ -3,13 +3,33 @@
 
 module Course.ListTest where
 
-import           Test.Tasty       (TestTree, testGroup)
-import           Test.Tasty.HUnit (testCase, (@?=))
+import           Test.Tasty            (TestTree, testGroup)
+import           Test.Tasty.HUnit      (testCase, (@?=))
+import           Test.Tasty.QuickCheck (testProperty)
 
 import           Course.Core
+import           Course.List           (List (..), headOr, infinity, product)
 
 test_List :: TestTree
 test_List =
   testGroup "List" [
-    testCase "placeholder" $ 1 @?= 1
+    headOrTest
+  , productTest
+  ]
+
+headOrTest :: TestTree
+headOrTest =
+  testGroup "headOr" [
+    testCase "headOr on non-empty list" $ headOr 3 (1 :. 2 :. Nil) @?= 1
+  , testCase "headOr on empty list" $ headOr 3 Nil @?= 3
+  , testProperty "headOr on infinity always 0" $ \x -> x `headOr` infinity == 0
+  , testProperty "headOr on empty list always the default" $ \x -> x `headOr` Nil == (x :: Integer)
+  ]
+
+productTest :: TestTree
+productTest =
+  testGroup "productTest" [
+    testCase "product of empty list" $ product Nil @?= 1
+  , testCase "product of 1..3" $ product (1 :. 2 :. 3 :. Nil) @?= 6
+  , testCase "product of 1..4" $ product (1 :. 2 :. 3 :. 4 :. Nil) @?= 24
   ]
