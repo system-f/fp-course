09ecb5d2c0eb58166d51c79ea3a1f19f5bae2e1e
Andrew McCluskey <andrew.mccluskey@csiro.au>
2017-07-03 11:02:53 +1000

    Finish OptionalTest

--- test/Course/OptionalTest.hs
+++ test/Course/OptionalTest.hs
@@ -7,13 +7,15 @@ import           Test.Tasty.HUnit      (testCase, (@?=))
 import           Test.Tasty.QuickCheck (testProperty)
 
 import Course.Core
-import Course.Optional (Optional (..), mapOptional, bindOptional)
+import Course.Optional (Optional (..), mapOptional, bindOptional, (??), (<+>))
 
 test_Optional :: TestTree
 test_Optional =
   testGroup "Optional" [
     mapOptionalTest
   , bindOptionalTest
+  , valueOrTest
+  , firstFullTest
   ]
 
 mapOptionalTest :: TestTree
@@ -35,4 +37,26 @@ bindOptionalTest =
           bindOptional evenDecOddInc (Full 8) @?= Full 7
       , testCase "even dec, odd inc, odd input" $
           bindOptional evenDecOddInc (Full 9) @?= Full 10
+  ]
+
+valueOrTest :: TestTree
+valueOrTest =
+  testGroup "??" [
+    testCase "Full" $
+      Full 8 ?? 99 @?= 8
+  , testCase "Empty" $
+      Empty ?? 99 @?= 99
+  ]
+
+firstFullTest :: TestTree
+firstFullTest =
+  testGroup "<+>" [
+    testCase "first Full" $
+      Full 8 <+> Empty @?= Full 8
+  , testCase "both Full" $
+      Full 8 <+> Full 9 @?= Full 8
+  , testCase "first Empty" $
+      Empty <+> Full 9 @?= Full 9
+  , testCase "both empty" $
+      Empty <+> Empty @?= (Empty :: Optional Integer)
   ]
