1c6f7529788e64d9c421db8317099e3fa8a43084
Tony Morris <tmorris@tmorris.net>
2017-07-07 06:39:51 +1000

    comments and tips for FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -79,35 +79,43 @@ main =
 type FilePath =
   Chars
 
--- /Tip:/ Use @getFiles@ and @printFiles@.
+-- Given a file name, read it and for each line in that file, read and print contents of each.
+-- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
 run =
   error "todo: Course.FileIO#run"
 
+-- Given a list of file names, return list of (file name and file contents).
+-- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
 getFiles =
   error "todo: Course.FileIO#getFiles"
 
+-- Given a file name, return (file name and file contents).
+-- Use @readFile@
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
 getFile =
   error "todo: Course.FileIO#getFile"
 
+-- Given a list of (file name and file contents), print each.
+-- Use @printFile@
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
 printFiles =
   error "todo: Course.FileIO#printFiles"
 
+-- Given the file name, and file contents, print them.
+-- Use @putStrLn@
 printFile ::
   FilePath
   -> Chars
   -> IO ()
 printFile =
   error "todo: Course.FileIO#printFile"
-
