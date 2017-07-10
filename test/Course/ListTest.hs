2e969b7847c05207808679cb801f6e0da201ee32
Andrew McCluskey <andrew@ajmccluskey.com>
2017-07-10 13:33:18 +1000

    Add list shrinkers

--- test/Course/ListTest.hs
+++ test/Course/ListTest.hs
@@ -6,7 +6,7 @@ module Course.ListTest where
 import           Control.Applicative   (liftA2, liftA3)
 import qualified Prelude               as P (fmap, foldr)
 
-import           Test.QuickCheck       (Arbitrary (..), Gen, forAll)
+import           Test.QuickCheck       (Arbitrary (..), Gen, forAllShrink)
 import           Test.Tasty            (TestTree, testGroup)
 import           Test.Tasty.HUnit      (testCase, (@?=))
 import           Test.Tasty.QuickCheck (testProperty)
@@ -14,9 +14,9 @@ import           Test.Tasty.QuickCheck (testProperty)
 import           Course.Core
 import           Course.List           (List (..), filter, find, flatMap,
                                         flatten, flattenAgain, foldLeft, headOr,
-                                        infinity, largeList, length, lengthGT4,
-                                        listh, map, produce, product, reverse,
-                                        seqOptional, sum, take, (++))
+                                        hlist, infinity, largeList, length,
+                                        lengthGT4, listh, map, produce, product,
+                                        reverse, seqOptional, sum, take, (++))
 import           Course.Optional       (Optional (..))
 
 test_List :: TestTree
@@ -62,7 +62,7 @@ sumTest =
     testCase "sum 1..3" $ sum (1 :. 2 :. 3 :. Nil) @?= 6
   , testCase "sum 1..4" $ sum (1 :. 2 :. 3 :. 4 :. Nil) @?= 10
   , testProperty "subtracting each element in a list from its sum is always 0" $
-      forAll genList (\x -> foldLeft (-) (sum x) x == 0)
+      forAllShrink genList shrinkList (\x -> foldLeft (-) (sum x) x == 0)
   ]
 
 lengthTest :: TestTree
@@ -70,7 +70,7 @@ lengthTest =
   testGroup "length" [
     testCase "length 1..3" $ length (1 :. 2 :. 3 :. Nil) @?= 3
   , testProperty "summing a list of 1s is equal to its length" $
-      forAll genIntegerList (\x -> sum (map (const 1) x) == length x)
+      forAllShrink genIntegerList shrinkList (\x -> sum (map (const 1) x) == length x)
   ]
 
 mapTest :: TestTree
@@ -81,7 +81,7 @@ mapTest =
   , testProperty "headOr after map" $
       \x -> headOr (x :: Integer) (map (+1) infinity) == 1
   , testProperty "map id is id" $
-      forAll genIntegerList (\x -> map id x == x)
+      forAllShrink genIntegerList shrinkList (\x -> map id x == x)
   ]
 
 filterTest :: TestTree
@@ -92,9 +92,9 @@ filterTest =
   , testProperty "filter (const True) is identity (headOr)" $
       \x -> headOr x (filter (const True) infinity) == 0
   , testProperty "filter (const True) is identity" $
-      forAll genIntegerList (\x -> filter (const True) x == x)
+      forAllShrink genIntegerList shrinkList (\x -> filter (const True) x == x)
   , testProperty "filter (const False) is the empty list" $
-      forAll genIntegerList (\x -> filter (const False) x == Nil)
+      forAllShrink genIntegerList shrinkList (\x -> filter (const False) x == Nil)
   ]
 
 appendTest :: TestTree
@@ -105,11 +105,11 @@ appendTest =
   , testProperty "append empty to infinite" $
       \x -> headOr x (Nil ++ infinity) == 0
   , testProperty "append anything to infinity" $
-       forAll genIntegerAndList (\(x, y) -> headOr x (y ++ infinity) == headOr 0 y)
+       forAllShrink genIntegerAndList shrinkIntegerAndList (\(x, y) -> headOr x (y ++ infinity) == headOr 0 y)
   , testProperty "associativity" $
-      forAll genThreeLists (\(x,y,z) -> (x ++ y) ++ z == x ++ (y ++ z))
+      forAllShrink genThreeLists shrinkThreeLists (\(x,y,z) -> (x ++ y) ++ z == x ++ (y ++ z))
   , testProperty "append to empty list" $
-      forAll genIntegerList (\x -> x ++ Nil == x)
+      forAllShrink genIntegerList shrinkList (\x -> x ++ Nil == x)
   ]
 
 flattenTest :: TestTree
@@ -118,11 +118,11 @@ flattenTest =
     testCase "(1..9)" $
       flatten ((1 :. 2 :. 3 :. Nil) :. (4 :. 5 :. 6 :. Nil) :. (7 :. 8 :. 9 :. Nil) :. Nil) @?= listh [1,2,3,4,5,6,7,8,9]
   , testProperty "flatten (infinity :. y)" $
-      forAll genIntegerAndList (\(x, y) -> headOr x (flatten (infinity :. y :. Nil)) == 0)
+      forAllShrink genIntegerAndList shrinkIntegerAndList (\(x, y) -> headOr x (flatten (infinity :. y :. Nil)) == 0)
   , testProperty "flatten (y :. infinity)" $
-      forAll genIntegerAndList (\(x, y) -> headOr x (flatten (y :. infinity :. Nil)) == headOr 0 y)
+      forAllShrink genIntegerAndList shrinkIntegerAndList (\(x, y) -> headOr x (flatten (y :. infinity :. Nil)) == headOr 0 y)
   , testProperty "sum of lengths == sum of flattened" $
-      forAll genListOfLists (\x -> sum (map length x) == length (flatten x))
+      forAllShrink genListOfLists shrinkListOfLists (\x -> sum (map length x) == length (flatten x))
   ]
 
 flatMapTest :: TestTree
@@ -131,18 +131,18 @@ flatMapTest =
     testCase "lists of Integer" $
       flatMap (\x -> x :. x + 1 :. x + 2 :. Nil) (1 :. 2 :. 3 :. Nil) @?= listh [1,2,3,2,3,4,3,4,5]
   , testProperty "flatMap id flattens a list of lists" $
-      forAll genIntegerAndList (\(x, y) -> headOr x (flatMap id (infinity :. y :. Nil)) == 0)
+      forAllShrink genIntegerAndList shrinkIntegerAndList (\(x, y) -> headOr x (flatMap id (infinity :. y :. Nil)) == 0)
   , testProperty "flatMap id on a list of lists take 2" $
-      forAll genIntegerAndList (\(x, y) -> headOr x (flatMap id (y :. infinity :. Nil)) == headOr 0 y)
+      forAllShrink genIntegerAndList shrinkIntegerAndList (\(x, y) -> headOr x (flatMap id (y :. infinity :. Nil)) == headOr 0 y)
   , testProperty "flatMap id == flatten" $
-      forAll genListOfLists (\x -> flatMap id x == flatten x)
+      forAllShrink genListOfLists shrinkListOfLists (\x -> flatMap id x == flatten x)
   ]
 
 flattenAgainTest :: TestTree
 flattenAgainTest =
   testGroup "flattenAgain" [
     testProperty "lists of Integer" $
-      forAll genListOfLists (\x -> flatten x == flattenAgain x)
+      forAllShrink genListOfLists shrinkListOfLists (\x -> flatten x == flattenAgain x)
   ]
 
 
@@ -195,9 +195,9 @@ reverseTest =
   , testCase "reverse . reverse on largeList" $
       take 1 (reverse (reverse largeList)) @?= (1 :. Nil)
   , testProperty "reverse then append is same as append then reverse" $
-      forAll genTwoLists (\(x, y) -> reverse x ++ reverse y == reverse (y ++ x))
+      forAllShrink genTwoLists shrinkTwoLists (\(x, y) -> reverse x ++ reverse y == reverse (y ++ x))
   , testProperty "" $
-      forAll genIntegerList (\x -> reverse (x :. Nil) == x :. Nil)
+      forAllShrink genIntegerList shrinkList (\x -> reverse (x :. Nil) == x :. Nil)
   ]
 
 produceTest :: TestTree
@@ -215,17 +215,33 @@ produceTest =
 genList :: Arbitrary a => Gen (List a)
 genList = P.fmap ((P.foldr (:.) Nil) :: [a] -> List a) arbitrary
 
+shrinkList :: Arbitrary a => List a -> [List a]
+shrinkList =
+  P.fmap listh . shrink . hlist
+
 genIntegerList :: Gen (List Integer)
 genIntegerList = genList
 
 genIntegerAndList :: Gen (Integer, List Integer)
 genIntegerAndList = P.fmap (P.fmap listh) arbitrary
 
+shrinkIntegerAndList :: (Integer, List Integer) -> [(Integer, List Integer)]
+shrinkIntegerAndList = P.fmap (P.fmap listh) . shrink . P.fmap hlist
+
 genTwoLists :: Gen (List Integer, List Integer)
 genTwoLists = liftA2 (,) genIntegerList genIntegerList -- (arbitrary :: (List Integer, List Integer))
+
+shrinkTwoLists :: (List Integer, List Integer) -> [(List Integer, List Integer)]
+shrinkTwoLists (a,b) = P.fmap (\(as,bs) -> (listh as, listh bs)) $ shrink (hlist a, hlist b)
 
 genThreeLists :: Gen (List Integer, List Integer, List Integer)
 genThreeLists = liftA3 (,,) genIntegerList genIntegerList genIntegerList
 
+shrinkThreeLists :: (List Integer, List Integer, List Integer) -> [(List Integer, List Integer, List Integer)]
+shrinkThreeLists (a,b,c) = P.fmap (\(as,bs,cs) -> (listh as, listh bs, listh cs)) $ shrink (hlist a, hlist b, hlist c)
+
 genListOfLists :: Gen (List (List Integer))
 genListOfLists = P.fmap (P.fmap listh) (genList :: (Gen (List [Integer])))
+
+shrinkListOfLists :: Arbitrary a => List (List a) -> [List (List a)]
+shrinkListOfLists = P.fmap (P.fmap listh). shrinkList . P.fmap hlist
