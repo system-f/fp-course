4deca7d888b1b170ed10c160fda3cfb59352900e
Dave Laing <dave.laing.80@gmail.com>
2017-07-19 16:18:40 +1000

    Wrangle things to test multiple compilers

--- ci/ci.nix
+++ ci/ci.nix
@@ -1,10 +1,30 @@
+{ supportedSystems ? ["x86_64-linux"]
+, supportedCompilers ? ["ghc7103" "ghc802"]
+}:
+
+with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
+
 let
   pkgs = import <nixpkgs> {};
 
-  fp-course = import ../default.nix {};
+  configurations = 
+    pkgs.lib.listToAttrs (
+      pkgs.lib.concatMap (compiler: 
+        pkgs.lib.concatMap (system: 
+          [{name = "fp-course_" + compiler + "_" + system; value = {inherit compiler system;};}]
+        ) supportedSystems
+      ) supportedCompilers
+    );
 
-  jobs = rec {
-    inherit fp-course;
-  };
+  jobs =
+      pkgs.lib.mapAttrs (name: configuration: 
+          let
+            compiler = configuration.compiler; 
+            system = configuration.system; 
+            nixpkgs = { pkgs = pkgsFor system; };
+            course = import ../default.nix { inherit nixpkgs compiler; };
+          in
+            course
+      ) configurations;
 in
   jobs
