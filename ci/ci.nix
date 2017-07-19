ecc800858591366259f2dd652c64494cc99e252f
Dave Laing <dave.laing.80@gmail.com>
2017-07-19 14:47:30 +1000

    Preparation for using Hydra for CI.

---
+++ ci/ci.nix
@@ -0,0 +1,30 @@
+{ supportedSystems ? ["x86_64-linux"]
+, supportedCompilers ? ["ghc7103" "ghc802"]
+}:
+
+with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
+
+let
+  pkgs = import <nixpkgs> {};
+
+  configurations = 
+    pkgs.lib.listToAttrs (
+      pkgs.lib.concatMap (compiler: 
+        pkgs.lib.concatMap (system: 
+          [{name = "fp-course_" + compiler + "_" + system; value = {inherit compiler system;};}]
+        ) supportedSystems
+      ) supportedCompilers
+    );
+
+  jobs =
+      pkgs.lib.mapAttrs (name: configuration: 
+          let
+            compiler = configuration.compiler; 
+            system = configuration.system; 
+            nixpkgs = { pkgs = pkgsFor system; };
+            course = import ../default.nix { inherit nixpkgs compiler; };
+          in
+            course
+      ) configurations;
+in
+  jobs
---
+++ ci/jobsets.nix
@@ -0,0 +1,27 @@
+{ nixpkgs, declInput }: let pkgs = import nixpkgs {}; in {
+  jobsets = pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toXML declInput}
+    EOF
+    cat > $out <<EOF
+    {
+        "fp-course": {
+            "enabled": 1,
+            "hidden": false,
+            "description": "FP Course",
+            "nixexprinput": "fp-course",
+            "nixexprpath": "./ci/ci.nix",
+            "checkinterval": 300,
+            "schedulingshares": 1,
+            "enableemail": false,
+            "emailoverride": "",
+            "keepnr": 5,
+            "inputs": {
+                "fp-course": { "type": "git", "value": "https://github.com/data61/fp-course.git master", "emailresponsible": false },
+                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-17.03", "emailresponsible": false }
+            }
+        }
+    }
+    EOF
+  '';
+}
