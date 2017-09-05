31a772658a277bf90f477a2d565b12a3345405be
Andrew McCluskey <andrew@ajmccluskey.com>
2017-09-06 09:23:34 +1000

    Add list test to ExtendTest

--- test/Course/ExtendTest.hs
+++ test/Course/ExtendTest.hs
@@ -9,6 +9,7 @@ import           Test.Tasty.HUnit  (testCase, (@?=))
 
 import           Course.Core
 import           Course.ExactlyOne (ExactlyOne (ExactlyOne))
+import           Course.List       (List (..), length)
 
 import           Course.Extend     ((<<=))
 
@@ -16,9 +17,17 @@ test_Extend :: TestTree
 test_Extend =
   testGroup "Extend" [
     exactlyOneTest
+  , listTest
   ]
 
 exactlyOneTest :: TestTree
 exactlyOneTest =
   testCase "ExactlyOne instance" $
     (id <<= ExactlyOne 7) @?= ExactlyOne (ExactlyOne 7)
+
+listTest :: TestTree
+listTest =
+  testGroup "List" [
+    testCase "length" $
+      (length <<= ('a' :. 'b' :. 'c' :. Nil)) @?= (3 :. 2 :. 1 :. Nil)
+  ]
