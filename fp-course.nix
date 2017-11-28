2b364bcb032a7fc4a9099087e49fdd747c59ddb1
George Wilson <george@wils.online>
2017-11-28 13:45:40 +1000

    Remove tasty-discover in favour of explicitly listing tests.

--- fp-course.nix
+++ fp-course.nix
@@ -1,6 +1,6 @@
 { mkDerivation, array, base, Cabal, cabal-doctest, containers
-, doctest, HUnit, QuickCheck, stdenv, tasty, tasty-discover
-, tasty-hunit, tasty-quickcheck
+, doctest, HUnit, QuickCheck, stdenv, tasty, tasty-hunit
+, tasty-quickcheck
 }:
 mkDerivation {
   pname = "course";
@@ -11,8 +11,7 @@ mkDerivation {
     array base containers doctest QuickCheck
   ];
   testHaskellDepends = [
-    base HUnit QuickCheck tasty tasty-discover tasty-hunit
-    tasty-quickcheck
+    base HUnit QuickCheck tasty tasty-hunit tasty-quickcheck
   ];
   homepage = "https://github.com/data61/fp-course";
   description = "Source code for a functional programming course";
