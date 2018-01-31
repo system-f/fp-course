0d4e9c48f1ae02daa4847be15f410c14db9cf5ff
Tony Morris <tmorris@tmorris.net>
2018-01-31 16:29:21 +1000

    reverse functions in FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -76,35 +76,14 @@ the contents of c
 
 -}
 
--- /Tip:/ use @getArgs@ and @run@
-main ::
-  IO ()
-main =
-  error "todo: Course.FileIO#main"
-
--- Given a file name, read it and for each line in that file, read and print contents of each.
--- Use @getFiles@ and @printFiles@.
-run ::
+-- Given the file name, and file contents, print them.
+-- Use @putStrLn@.
+printFile ::
   FilePath
+  -> Chars
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
-
--- Given a list of file names, return list of (file name and file contents).
--- Use @getFile@.
-getFiles ::
-  List FilePath
-  -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
-
--- Given a file name, return (file name and file contents).
--- Use @readFile@.
-getFile ::
-  FilePath
-  -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+printFile =
+  error "todo: Course.FileIO#printFile"
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
@@ -114,11 +93,32 @@ printFiles ::
 printFiles =
   error "todo: Course.FileIO#printFiles"
 
--- Given the file name, and file contents, print them.
--- Use @putStrLn@.
-printFile ::
+-- Given a file name, return (file name and file contents).
+-- Use @readFile@.
+getFile ::
   FilePath
-  -> Chars
+  -> IO (FilePath, Chars)
+getFile =
+  error "todo: Course.FileIO#getFile"
+
+-- Given a list of file names, return list of (file name and file contents).
+-- Use @getFile@.
+getFiles ::
+  List FilePath
+  -> IO (List (FilePath, Chars))
+getFiles =
+  error "todo: Course.FileIO#getFiles"
+
+-- Given a file name, read it and for each line in that file, read and print contents of each.
+-- Use @getFiles@ and @printFiles@.
+run ::
+  FilePath
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+run =
+  error "todo: Course.FileIO#run"
+
+-- /Tip:/ use @getArgs@ and @run@
+main ::
+  IO ()
+main =
+  error "todo: Course.FileIO#main"
