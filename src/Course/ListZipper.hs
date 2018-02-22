d3d5bf8ef78e400b573441e0151b5bc189dcc4d9
Yakov Pechersky <ffxen158@gmail.com>
2018-02-21 22:50:49 -0500

    add doctest example to findLeft to test proper reversal of broken list

--- src/Course/ListZipper.hs
+++ src/Course/ListZipper.hs
@@ -255,6 +255,9 @@ hasRight =
 --
 -- >>> findLeft (== 1) (zipper [1, 2, 1] 3 [4, 5])
 -- [2,1] >1< [3,4,5]
+--
+-- >>> findLeft (== 1) (zipper [4, 3, 2, 1, 2, 1] 3 [4, 5])
+-- [2,1] >1< [2,3,4,3,4,5]
 findLeft ::
   (a -> Bool)
   -> ListZipper a
