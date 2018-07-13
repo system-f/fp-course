80659de321cebc74b35c3ae9815f49d65793ebd7
Andrew McCluskey <andrew@ajmccluskey.com>
2018-07-13 10:15:02 +1000

    Write first cut of embedded tests.

--- test/Test/Mini.hs
+++ test/Test/Mini.hs
@@ -1,50 +1,78 @@
-{-# LANGUAGE ImplicitPrelude #-}
-{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ImplicitPrelude        #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TypeFamilies           #-}
 
--- | The smallest possible test library that will run the current test suite
+-- | The smallest possible test library interface and instance that will run the current test suite.
+-- For those comfortable with cabal, we also provide a Test.Tasty.Mini module with a type and
+-- instance for running the test suite with Tasty.
 module Test.Mini where
 
-import Data.Bool (bool)
-import Data.Foldable (traverse_)
+import           Control.Exception (SomeException, try)
+import           Data.Bool         (bool)
+import           Data.Foldable     (traverse_)
+import           Data.List         (intercalate)
+import           Data.Monoid       ((<>))
+import           Data.String       (IsString, fromString)
 
-class Tester t where
-  data TestName t
+-- | Test interface required to run course tests
+class IsString name => Tester t name | t -> name where
   data TestTree t
   data Assertion t
 
-  testGroup :: TestName t -> [TestTree t] -> TestTree t
-  testCase :: TestName t -> Assertion t -> TestTree t
+  testGroup :: name -> [TestTree t] -> TestTree t
+  testCase :: name -> Assertion t -> TestTree t
   (@?=) :: (Eq a, Show a) => a -> a -> Assertion t
-  test' :: TestTree t -> [TestName t]
-  test :: TestTree t -> IO ()
+  test :: TestTree t -> IO [name]
 
-data CourseTester =
-  CourseTester
 
+-- | A data type for our embedded instance to hang off
+data CourseTester
+
+-- | The test tree structure used by our embedded instance
 data CourseTestTree =
-  Single { name :: String, assertion :: Bool }
+  Single { name :: String, assertion :: IO () }
   | Tree { name :: String, testCases :: [CourseTestTree]}
 
+-- | Run our embedded test tree, printing failures.
 testCourseTree' ::
-  CourseTestTree -> [String]
-testCourseTree' t = case t of
-  (Single s a) -> bool [s] [] a
-  (Tree _ ts) -> foldMap testCourseTree' ts
+  CourseTestTree -> IO [String]
+testCourseTree' =
+  go ""
+  where
+    go s t' =
+      case t' of
+      (Single s' a) -> do
+        r <- try a :: IO (Either SomeException ())
+        let
+          qualifiedName = bool (intercalate "." [s,s']) s' (null s)
+          quote x = "'" <> x <> "'"
+          printFailure e =
+            putStrLn (quote qualifiedName <> " failed:")
+            >> print e
+            >> putStrLn ""
+        case r of
+          Left e   -> printFailure e >> (pure . pure) qualifiedName
+          Right () -> pure []
+      (Tree s' ts) -> foldMap (go s') ts
 
-instance Tester CourseTester where
-  data TestName CourseTester = CTName String
+-- | Instance for the embedded test implementation
+instance Tester CourseTester String where
   data TestTree CourseTester = CTTree CourseTestTree
-  data Assertion CourseTester = CTAssertion Bool
+  data Assertion CourseTester = CTAssertion (IO ())
 
-  testGroup (CTName s) =
+  testGroup s =
     foldr1 (\(CTTree t1) (CTTree t2) -> CTTree (Tree s [t1, t2]))
 
-  testCase (CTName s) (CTAssertion a) = CTTree (Single s a)
+  testCase s (CTAssertion a) = CTTree (Single s a)
 
   a @?= b =
-    CTAssertion (a == b)
+    let
+      msg :: Prelude.String
+      msg = "Expected " <> show a <> " but got " <> show b
+    in
+      CTAssertion $ bool (error msg) (pure ()) (a == b)
 
-  test' (CTTree t) =
-    CTName <$> testCourseTree' t
-
-  test = traverse_ (\(CTName s) -> putStrLn s) . test'
+  test (CTTree t) =
+    testCourseTree' t
