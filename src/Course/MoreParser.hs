3435f3ea8bd6d4adb730b27b11c33d48a7377188
Brian McKenna <brian@brianmckenna.org>
2018-07-26 08:34:36 +1000

    Fix error typo in Course.MoreParser#string

--- src/Course/MoreParser.hs
+++ src/Course/MoreParser.hs
@@ -116,7 +116,7 @@ string ::
   Chars
   -> Parser Chars
 string =
-  error "todo: Course.MoreParser#is"
+  error "todo: Course.MoreParser#string"
 
 -- | Write a function that parsers the given string, followed by 0 or more spaces.
 --
