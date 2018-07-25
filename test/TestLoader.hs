1974ce487d18320c2cfc6bff26fb3ab6a723d56c
Andrew McCluskey <andrew@ajmccluskey.com>
2018-07-25 16:26:31 +1000

    Reenable tests while fixing remaining issues

--- test/TestLoader.hs
+++ test/TestLoader.hs
@@ -2,17 +2,17 @@
 
 module TestLoader where
 
--- import           Course.ApplicativeTest (test_Applicative)
--- import           Course.ComonadTest     (test_Comonad)
--- import           Course.ExtendTest      (test_Extend)
--- import           Course.FunctorTest     (test_Functor)
--- import           Course.JsonParserTest  (test_JsonParser)
--- import           Course.ListTest        (test_List)
--- import           Course.ListZipperTest  (test_ListZipper)
--- import           Course.MonadTest       (test_Monad)
--- import           Course.OptionalTest    (test_Optional)
--- import           Course.StateTest       (test_State)
--- import           Course.StateTTest      (test_StateT)
+import           Course.ApplicativeTest (test_Applicative)
+import           Course.ComonadTest     (test_Comonad)
+import           Course.ExtendTest      (test_Extend)
+import           Course.FunctorTest     (test_Functor)
+import           Course.JsonParserTest  (test_JsonParser)
+import           Course.ListTest        (test_List)
+import           Course.ListZipperTest  (test_ListZipper)
+import           Course.MonadTest       (test_Monad)
+import           Course.OptionalTest    (test_Optional)
+import           Course.StateTest       (test_State)
+import           Course.StateTTest      (test_StateT)
 import           Course.ValidationTest (test_Validation)
 
 import           Data.String           (fromString)
@@ -24,17 +24,17 @@ import           Test.Mini             (MiniTestTree, Tester (..),
 tests :: MiniTestTree
 tests =
   testGroup "Tests" [
-  --   test_Optional
-  -- , test_List
-  -- , test_Functor
-  -- , test_Applicative
-  -- , test_Monad
-  -- , test_State
-  -- , test_StateT
-    test_Validation
-  -- , test_Extend
-  -- , test_Comonad
-  -- , test_ListZipper
-  -- , test_JsonParser
+    test_Optional
+  , test_List
+  , test_Functor
+  , test_Applicative
+  , test_Monad
+  , test_State
+  , test_StateT
+  , test_Validation
+  , test_Extend
+  , test_Comonad
+  , test_ListZipper
+  , test_JsonParser
   ]
 
