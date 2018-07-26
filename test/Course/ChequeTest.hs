c12016f4249dc4d3ec6b880ac4499a3b3b6d624a
jack <steve.a.shogren@gmail.com>
2018-07-26 16:45:04 -0400

    Tweaking the cheque tests

--- test/Course/ChequeTest.hs
+++ test/Course/ChequeTest.hs
@@ -22,49 +22,50 @@ chequeDollarsTest =
   testGroup "jsonObject" [
   testCase "empty" $
       dollars "0" @?= "zero dollars and zero cents"
-    testCase "dollars '1'" $
+  , testCase "dollars '1'" $
       dollars "1" @?=  "one dollar and zero cents"
-    testCase "dollars '0.1'" $
+  , testCase "dollars '0.1'" $
       dollars "0.1" @?=  "zero dollars and ten cents"
-    testCase "dollars '1.'" $
+  , testCase "dollars '1.'" $
       dollars "1." @?=  "one dollar and zero cents"
-    testCase "dollars '0.'" $
+  , testCase "dollars '0.'" $
       dollars "0." @?=  "zero dollars and zero cents"
-    testCase "dollars '0.0'" $
+  , testCase "dollars '0.0'" $
       dollars "0.0" @?=  "zero dollars and zero cents"
-    testCase "dollars '.34'" $
+  , testCase "dollars '.34'" $
       dollars ".34" @?=  "zero dollars and thirty-four cents"
-    testCase "dollars '0.3456789'" $
+  , testCase "dollars '0.3456789'" $
       dollars "0.3456789" @?=  "zero dollars and thirty-four cents"
-    testCase "dollars '1.0'" $
+  , testCase "dollars '1.0'" $
       dollars "1.0" @?=  "one dollar and zero cents"
-    testCase "dollars '1.01'" $
+  , testCase "dollars '1.01'" $
       dollars "1.01" @?=  "one dollar and one cent"
-    testCase "dollars 'a1a'" $
+  , testCase "dollars 'a1a'" $
       dollars "a1a" @?=  "one dollar and zero cents"
-    testCase "dollars 'a1a.a0.7b'" $
+  , testCase "dollars 'a1a.a0.7b'" $
       dollars "a1a.a0.7b" @?=  "one dollar and seven cents"
-    testCase "dollars '100'" $
+  , testCase "dollars '100'" $
       dollars "100" @?=  "one hundred dollars and zero cents"
-    testCase "dollars '100.0'" $
+  , testCase "dollars '100.0'" $
       dollars "100.0" @?=  "one hundred dollars and zero cents"
-    testCase "dollars '100.00'" $
+  , testCase "dollars '100.00'" $
       dollars "100.00" @?=  "one hundred dollars and zero cents"
-    testCase "dollars '100.00000'" $
+  , testCase "dollars '100.00000'" $
       dollars "100.00000" @?=  "one hundred dollars and zero cents"
-    testCase "dollars '1000456.13'" $
+  , testCase "dollars '1000456.13'" $
       dollars "1000456.13" @?=  "one million four hundred and fifty-six dollars and thirteen cents"
-    testCase "dollars '1001456.13'" $
+  , testCase "dollars '1001456.13'" $
       dollars "1001456.13" @?=  "one million one thousand four hundred and fifty-six dollars and thirteen cents"
-    testCase "dollars '16000000456.13'" $
+  , testCase "dollars '16000000456.13'" $
       dollars "16000000456.13" @?=  "sixteen billion four hundred and fifty-six dollars and thirteen cents"
-    testCase "dollars '100.45'" $
+  , testCase "dollars '100.45'" $
       dollars "100.45" @?=  "one hundred dollars and forty-five cents"
-    testCase "dollars '100.07'" $
+  , testCase "dollars '100.07'" $
       dollars "100.07" @?=  "one hundred dollars and seven cents"
-    testCase "dollars '9abc9def9ghi.jkl9mno'" $
+  , testCase "dollars '9abc9def9ghi.jkl9mno'" $
       dollars "9abc9def9ghi.jkl9mno" @?=  "nine hundred and ninety-nine dollars and ninety cents"
-    testCase "dollars '12345.67'" $
+  , testCase "dollars '12345.67'" $
       dollars "12345.67" @?=  "twelve thousand three hundred and forty-five dollars and sixty-seven cents"
-    testCase "dollars '456789123456789012345678901234567890123456789012345678901234567890.12'" $
+  , testCase "dollars '456789123456789012345678901234567890123456789012345678901234567890.12'" $
       dollars "456789123456789012345678901234567890123456789012345678901234567890.12" @?=  "four hundred and fifty-six vigintillion seven hundred and eighty-nine novemdecillion one hundred and twenty-three octodecillion four hundred and fifty-six septendecillion seven hundred and eighty-nine sexdecillion twelve quindecillion three hundred and forty-five quattuordecillion six hundred and seventy-eight tredecillion nine hundred and one duodecillion two hundred and thirty-four undecillion five hundred and sixty-seven decillion eight hundred and ninety nonillion one hundred and twenty-three octillion four hundred and fifty-six septillion seven hundred and eighty-nine sextillion twelve quintillion three hundred and forty-five quadrillion six hundred and seventy-eight trillion nine hundred and one billion two hundred and thirty-four million five hundred and sixty-seven thousand eight hundred and ninety dollars and twelve cents"
+      ]
