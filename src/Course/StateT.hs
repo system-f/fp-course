d82d69753bf7e8eac7c1d22733d688807c2b2065
Tony Morris <tmorris@tmorris.net>
2019-03-12 13:41:30 +1000

    fix test

--- src/Course/StateT.hs
+++ src/Course/StateT.hs
@@ -95,7 +95,7 @@ type State' s a =
 -- | Provide a constructor for `State'` values
 --
 -- >>> runStateT (state' $ runState $ put 1) 0
--- ExactlyOne  ((),1)
+-- ExactlyOne ((),1)
 state' ::
   (s -> (a, s))
   -> State' s a
