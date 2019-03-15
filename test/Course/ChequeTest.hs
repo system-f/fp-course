a55b431bb42ee98a7ce41a10d830ba84f061cab8
Andrew McCluskey <andrew@ajmccluskey.com>
2019-03-15 10:31:24 +1000

    Fix up ChequeTest
    - Fix group name
    - rename test
    - import/export courseTest

--- test/Course/ChequeTest.hs
+++ test/Course/ChequeTest.hs
@@ -2,24 +2,30 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Course.ChequeTest where
+module Course.ChequeTest (
+    test_Cheque
+  , dollarsTest
 
-import           Test.Mini  (testCase, (@?=), MiniTestTree, testGroup)
+  , courseTest
+  ) where
 
+import           Test.Course.Mini (courseTest)
+import           Test.Mini        (MiniTestTree, testCase, testGroup, (@?=))
+
+import           Course.Cheque    (dollars)
 import           Course.Core
-import           Course.Cheque (dollars)
-import           Course.List       (List (..))
+import           Course.List      (List (..))
 
 test_Cheque :: MiniTestTree
 test_Cheque =
   testGroup "Cheque" [
-    chequeDollarsTest
+    dollarsTest
   ]
 
-chequeDollarsTest :: MiniTestTree
-chequeDollarsTest =
-  testGroup "jsonObject" [
-  testCase "empty" $
+dollarsTest :: MiniTestTree
+dollarsTest =
+  testGroup "dollars" [
+    testCase "empty" $
       dollars "0" @?= "zero dollars and zero cents"
   , testCase "dollars '1'" $
       dollars "1" @?=  "one dollar and zero cents"
