b446322e64dfdf0bf3579ad7c6d986af15653255
Andrew McCluskey <andrew@ajmccluskey.com>
2019-04-08 13:58:12 +1000

    Use commit from unstable. Nothing from 18.09 works?

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 import (builtins.fetchGit {
   # Descriptive name to make the store path easier to identify
-  name = "nixos-18.09-2019-04-08";
-  url = https://github.com/nixos/nixpkgs;
-  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-18.09`
-  rev = "222950952f15f6b1e9f036b80440b597f23e652d";
+  name = "nixos-unstable-2019-04-08";
+  url = https://github.com/nixos/nixpkgs/;
+  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
+  rev = "acbdaa569f4ee387386ebe1b9e60b9f95b4ab21b";
 }) {}
