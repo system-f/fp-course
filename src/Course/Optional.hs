13e52d01c7c77aca9d8e5ba1ce411990c54c9c3d
Sergei Kharchikov <s.kharchikov@tinkoff.ru>
2022-08-16 20:16:22 +0300

    Solve exercises in Optional

--- src/Course/Optional.hs
+++ src/Course/Optional.hs
@@ -7,13 +7,12 @@ import qualified Control.Applicative as A
 import qualified Control.Monad as M
 import Course.Core
 import qualified Prelude as P
+import Data.Void (vacuous)
 
 -- | The `Optional` data type contains 0 or 1 value.
 --
 -- It might be thought of as a list, with a maximum length of one.
-data Optional a =
-  Full a
-  | Empty
+data Optional a = Full a | Empty
   deriving (Eq, Show)
 
 -- | Return the possible value if it exists; otherwise, the first argument.
@@ -23,12 +22,9 @@ data Optional a =
 --
 -- >>> fullOr 99 Empty
 -- 99
-fullOr ::
-  a
-  -> Optional a
-  -> a
-fullOr =
-  error "todo: Course.Optional#fullOr"
+fullOr :: a -> Optional a -> a
+fullOr _ (Full a) = a
+fullOr a Empty = a
 
 -- | Map the given function on the possible value.
 --
@@ -37,12 +33,9 @@ fullOr =
 --
 -- >>> mapOptional (+1) (Full 8)
 -- Full 9
-mapOptional ::
-  (a -> b)
-  -> Optional a
-  -> Optional b
-mapOptional =
-  error "todo: Course.Optional#mapOptional"
+mapOptional :: (a -> b) -> Optional a -> Optional b
+mapOptional _ Empty = Empty
+mapOptional f (Full a) = Full (f a)
 
 -- | Bind the given function on the possible value.
 --
@@ -54,12 +47,9 @@ mapOptional =
 --
 -- >>> bindOptional (\n -> if even n then Full (n - 1) else Full (n + 1)) (Full 9)
 -- Full 10
-bindOptional ::
-  (a -> Optional b)
-  -> Optional a
-  -> Optional b
-bindOptional =
-  error "todo: Course.Optional#bindOptional"
+bindOptional :: (a -> Optional b) -> Optional a -> Optional b
+bindOptional _ Empty    = Empty
+bindOptional f (Full a) = f a
 
 -- | Try the first optional for a value. If it has a value, use it; otherwise,
 -- use the second value.
@@ -75,12 +65,9 @@ bindOptional =
 --
 -- >>> Empty <+> Empty
 -- Empty
-(<+>) ::
-  Optional a
-  -> Optional a
-  -> Optional a
-(<+>) =
-  error "todo: Course.Optional#(<+>)"
+(<+>) :: Optional a -> Optional a -> Optional a
+Full a <+> _ = Full a
+Empty <+> o  = o
 
 -- | Replaces the Full and Empty constructors in an optional.
 --
@@ -89,13 +76,9 @@ bindOptional =
 --
 -- >>> optional (+1) 0 Empty
 -- 0
-optional ::
-  (a -> b)
-  -> b
-  -> Optional a
-  -> b
-optional =
-  error "todo: Course.Optional#optional"
+optional :: (a -> b) -> b -> Optional a -> b
+optional f _ (Full a) = f a
+optional _ z Empty    = z 
 
 applyOptional :: Optional (a -> b) -> Optional a -> Optional b
 applyOptional f a = bindOptional (\f' -> mapOptional f' a) f
