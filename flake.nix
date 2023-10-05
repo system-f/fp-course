2f18d55c6373942b3c50808f5895347d92213f98
Josh Godsiff <jgodsiff@seek.com.au>
2023-10-05 16:07:34 +1100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Example Haskell development environment for Zero to Nix";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs.haskellPackages; [
+            cabal-install
+            ghc
+            haskell-language-server
+            hlint
+          ];
+        };
+      });
+    };
+}
